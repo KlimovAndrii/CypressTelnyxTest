8caf7ccc14d85005f070bc6f025c5cde7f43eb55
KlimovAndrii <ag.klimov@gmail.com>
2022-09-26 15:57:21 +0300

    modified locators

--- cypress/pages/userpage.page.js
+++ cypress/pages/userpage.page.js
@@ -1,7 +1,7 @@
 import Page from "./page";
 
 class UserPage extends Page {
-    userPageHeaderLocator = `//div[@data-testid="flex" and @class="Flex__FlexComponent-fGtahC fmDnDf tx-2c709E"]`;
+    userPageHeaderLocator = `//h1[text()="Numbers / Search & Buy Numbers"]`;
 
     isPageOpened() { super.isPageOpened(this.userPageHeaderLocator); }
 }
